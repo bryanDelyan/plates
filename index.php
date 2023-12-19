cb7c452ee06f01ea4f1ec15474f6c91b46b3cba6
Sebastian Vargaz <juancbastianrv@gmail.com>
2023-12-19 10:51:52 -0500

    d

--- index.php
+++ index.php
@@ -485,23 +485,6 @@ label {
 
     gtag('config', 'G-WRHGX2FHE7');
     </script>
-    <!-- Hotjar Tracking Code for https://serviplatexpressusa.com/ -->
-    <script>
-    (function(h, o, t, j, a, r) {
-        h.hj = h.hj || function() {
-            (h.hj.q = h.hj.q || []).push(arguments)
-        };
-        h._hjSettings = {
-            hjid: 3800497,
-            hjsv: 6
-        };
-        a = o.getElementsByTagName('head')[0];
-        r = o.createElement('script');
-        r.async = 1;
-        r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
-        a.appendChild(r);
-    })(window, document, 'https://static.hotjar.com/c/hotjar-', '.js?sv=');
-    </script>
 </head>
 
 <body>
