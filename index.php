2ccfaad1149aaa9f1d1665ce3368c4308f5a2724
Sebastian Vargaz <juancbastianrv@gmail.com>
2023-12-19 10:28:35 -0500

    hotjar

--- index.php
+++ index.php
@@ -421,6 +421,7 @@ label {
 </style>
 <!DOCTYPE html>
 <html lang="en-us">
+
 <head>
     <!-- SEO OPEN -->
     <title>ServiPlates Express USA - Placas Temporales, Smog, Inspecciones y Seguros</title>
@@ -483,6 +484,23 @@ label {
     gtag('js', new Date());
 
     gtag('config', 'G-WRHGX2FHE7');
+    </script>
+    <!-- Hotjar Tracking Code for https://serviplatexpressusa.com/ -->
+    <script>
+    (function(h, o, t, j, a, r) {
+        h.hj = h.hj || function() {
+            (h.hj.q = h.hj.q || []).push(arguments)
+        };
+        h._hjSettings = {
+            hjid: 3800497,
+            hjsv: 6
+        };
+        a = o.getElementsByTagName('head')[0];
+        r = o.createElement('script');
+        r.async = 1;
+        r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window, document, 'https://static.hotjar.com/c/hotjar-', '.js?sv=');
     </script>
 </head>
 
