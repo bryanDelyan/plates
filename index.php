62f5d5a5fa242e5b4620ea9ea1d6d240ce5e55d0
Sebastian Vargaz <juancbastianrv@gmail.com>
2024-01-10 09:39:31 -0500

    SEO

--- index.php
+++ index.php
@@ -424,7 +424,7 @@ label {
 
 <head>
     <!-- SEO OPEN -->
-    <title>Placas Temporales</title>
+    <title>placas Temporales</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description"
@@ -435,27 +435,27 @@ label {
     <link rel="canonical" href="https://serviplatexpressusa.com">
     <link rel="alternate" hreflang="en" href="https://serviplatexpressusa.com/english">
     <link rel="alternate" hreflang="es" href="https://serviplatexpressusa.com">
-    <meta name="title" content="Placas temporales">
+    <meta name="title" content="placas temporales">
     <meta name="language" content="Español">
     <meta name="revisit-after" content="1 days">
-    <meta name="author" content="Placas Temporales">
+    <meta name="author" content="placas Temporales">
 
     <meta property="og:locale" content="es_ES">
     <meta property="og:type" content="website">
-    <meta property="og:title" content="Placas Temporales">
+    <meta property="og:title" content="placas Temporales">
     <meta property="og:description"
         content="placas temporales cerca de mi, te facilitamos el proceso de obtención de placas temporales de manera fácil y rápida.">
     <meta property="og:image" content="https://serviplatexpressusa.com/img/track.png">
     <meta property="og:image:secure_url" content="https://serviplatexpressusa.com/img/track.png">
     <meta property="og:url" content="https://serviplatexpressusa.com/">
-    <meta property="og:site_name" content="Placas Temporales">
+    <meta property="og:site_name" content="placas Temporales">
     <meta property="og:type" content="article" />
     <meta property="og:image:width" content="1200">
     <meta property="og:image:height" content="630">
     <meta property="article:published_time" content="2023-02-16T01:19:34+00:00">
     <meta property="article:modified_time" content="2024-01-10T22:18:53+00:00">
     <meta name="twitter:card" content="summary_large_image">
-    <meta name="twitter:title" content="Placas Temporales">
+    <meta name="twitter:title" content="placas Temporales">
     <meta name="twitter:description" content="¿Necesitas placas temporales para tu vehículo? En nuestra página, te facilitamos el proceso de obtención de placas temporales del DMV de manera fácil y rápida.">
     <meta name="twitter:image" content="https://placastemporalesdmv.com/wp-content/uploads/2023/09/cropped-Sin-titulo-3-1.png">
     <meta name="twitter:label1" content="Escrito por">
@@ -570,7 +570,7 @@ label {
                 <div class="row">
                     <div class="col-12 col-md-12 col-lg-7 pt-md-5">
                         <div class="info-text text-light">
-                            <h1>Placas Temporales en los Estados Unidos</h1>
+                            <h1>placas Temporales en los Estados Unidos</h1>
                             <p class="text-white">
                                 Experimenta la conveniencia incomparable de ServiPlates Express, tu solución de
                                 confianza para placas temporales
@@ -598,19 +598,19 @@ label {
             <div class="my-5 info-text2 py-5">
                 <div class="row">
                     <div class="col-12 col-md-6" style="text-align: justify;">
-                        <h2 class="mb-4">Placas Temporales</h2>
-                        <p class="mb-5">Embárcate en una aventura sin problemas con nosotros, la principal autoridad en
+                        <h2 class="mb-4">placas Temporales</h2>
+                        <h3 class="mb-5 fs-6 fw-normal">Embárcate en una aventura sin problemas con nosotros, la principal autoridad en
                             la emisión de
                             placas temporales en todo Estados Unidos. Con un compromiso inquebrantable, nos dedicamos a
                             servir a comunidades
-                            a nivel nacional con servicios transparentes y legalmente sólidos.</p>
-                        <p>Como pioneros en la industria, ofrecemos más que simples placas temporales; te brindamos
+                            a nivel nacional con servicios transparentes y legalmente sólidos.</h3>
+                        <h4 class="fs-6 fw-normal">Como pioneros en la industria, ofrecemos más que simples placas temporales; te brindamos
                             tranquilidad. Confía en
                             nosotros para una experiencia confiable, legal y libre de problemas. Nuestro compromiso va
                             más allá de proporcionar
                             placas temporales; contribuimos activamente a la reducción de la contaminación y promovemos
                             la responsabilidad
-                            ambiental.</p>
+                            ambiental.</h4>
                     </div>
                     <div class="col-12 col-md-6 text-end info-img2 mr-5">
                         <div class="px-lg-3 py-4 py-md-2 mx-md-3 text-end ">
@@ -626,7 +626,7 @@ label {
         <div class="p-md-5 p-3" style="   background-image: url('https://themewagon.github.io/AI-html/img/footer.png') !important;
             background-size: cover !important;">
             <div class="col-12 mb-2 info-text2 py-5 pb-2">
-                <h3 class="text-center">Embárcate en un Viaje de Diseños Únicos de Placas Temporales</h3>
+                <h1 class="text-center">Embárcate en un Viaje de Diseños Únicos de placas Temporales</h1>
                 <p class="text-center px-5">Descubre un mundo de placas temporales distintivas diseñadas para elevar tu
                     experiencia de
                     conducción. Sumérgete en lo extraordinario mientras fusionamos la innovación y el estilo de manera
@@ -658,7 +658,7 @@ label {
     <section class="container-fluid pb-5 bg-white" id="plates">
         <div class="p-md-5 p-3">
             <div class="col-12 mb-2 info-text2 py-5 pb-0">
-                <h4 class="text-center">Verificaciones de Smog e Inspecciones de Vehículos</h4>
+                <h1 class="text-center">Verificaciones de Smog e Inspecciones de Vehículos</h1>
             </div>
             <div class="container left">
                 <div class="row mt-0 gx-4 gy-2">
@@ -704,7 +704,7 @@ label {
         <div class="py-md-5" style="
             background-image: url('https://themewagon.github.io/AI-html/img/footer.png') !important;
             background-size: cover !important;">
-            <h5 class="text-center pt-5 pb-5 text-white mb-3">ESTADOS DONDE EXPEDIMOS PLACAS TEMPORALES</h5>
+            <h5 class="text-center pt-5 pb-5 text-white mb-3">ESTADOS DONDE EXPEDIMOS pLACAS TEMPORALES</h5>
             <div class="container pb-4 p-0">
                 <div class="row p-0">
                     <?php
